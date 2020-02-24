eacb2ed89dad88c2e1a6de6230bba1ca86dd21aa
ericposas <web_dev@tutanota.com>
2020-02-24 17:53:36 -0600

    small edits

--- express-src/utils/validateTokenMiddleware.js
+++ express-src/utils/validateTokenMiddleware.js
@@ -6,10 +6,11 @@ const validateTokenMiddleware = (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1] // Bearer <token>
     const options = { expiresIn: '2d', issuer: 'jwt-login-system' }
     try {
-      req.decoded = jwt.verify(token, process.env.JWT_SECRET, options)
+      jwt.verify(token, process.env.JWT_SECRET, options)
       next()
     } catch (err) {
-      throw new Error(err)
+      // throw new Error(err)
+      res.send({ error: 'invalid token' })
     }
   } else {
     res.send({ error: 'authentication error.' })
