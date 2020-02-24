4e7d3a93c736ac66242bf591085250fde297211c
ericposas <web_dev@tutanota.com>
2020-02-24 17:54:47 -0600

    added note about validation middleware

--- express-src/utils/validateTokenMiddleware.js
+++ express-src/utils/validateTokenMiddleware.js
@@ -5,6 +5,8 @@ const validateTokenMiddleware = (req, res, next) => {
   if (authorizationHeader) {
     const token = req.headers.authorization.split(' ')[1] // Bearer <token>
     const options = { expiresIn: '2d', issuer: 'jwt-login-system' }
+    // put jwt 'verify' function within the try block in order to catch
+    // validation hours
     try {
       jwt.verify(token, process.env.JWT_SECRET, options)
       next()
